3618827a3bc61213ee195b7d4e2e7f120b4289ff
Ryuheyhey <ryuhei.330.800m@icloud.com>
2021-01-20 18:38:48 +0900

    First commit

--- src/world.ts
+++ src/world.ts
@@ -1,15 +1,14 @@
 export default class World {
   // 型定義
-  massage: string;
+  massage: string
 
   constructor(massage: string) {
     this.massage = massage
   }
 
-  public sayHellow(elem:HTMLElement | null) {
+  public sayHellow(elem: HTMLElement | null) {
     if (elem) {
       elem.innerText = this.massage
     }
   }
 }
-
